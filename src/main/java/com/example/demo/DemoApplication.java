5a72b9744f8521fc0192980f52d2503761251be9
jiseon723 <dighxxx12@gmail.com>
2025-09-18 16:16:48 +0900

    article domain set_2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //DB에 생성일, 수정일 자동 기록
 @SpringBootApplication
 public class DemoApplication {
 
