eb9284bba9382ae773d7e973fff3b6db88b49c4e
jiseon723 <dighxxx12@gmail.com>
2025-09-18 15:38:48 +0900

    article domain set

---
+++ src/main/java/com/example/demo/global/jpa/BaseEntity.java
@@ -0,0 +1,31 @@
+package com.example.demo.global.jpa;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass //맵핑 정보를 가진 클래스라는걸 인식시킴
+@NoArgsConstructor(access = AccessLevel.PROTECTED) //기본생성자를 PROTECTED로 사용
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
