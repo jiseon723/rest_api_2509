08a833add773b32d8a38bfd79d97552c691ed37a
jiseon723 <dighxxx12@gmail.com>
2025-09-19 17:24:34 +0900

    response, request

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -2,13 +2,13 @@ package com.example.demo.article.controller;
 
 import com.example.demo.article.dto.ArticleDTO;
 import com.example.demo.article.entity.Article;
+import com.example.demo.article.request.ArticleCreateRequest;
+import com.example.demo.article.request.ArticleModifyRequest;
+import com.example.demo.article.response.ArticleResponse;
+import com.example.demo.article.response.ArticlesResponse;
 import com.example.demo.article.service.ArticleService;
 import com.example.demo.global.RsData.RsData;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,12 +22,6 @@ public class ApiV1ArticleController {
     private final ArticleService articleService;
 
 //    다건 조회
-    @Getter
-    @AllArgsConstructor
-    public static class ArticlesResponse {
-        private final List<ArticleDTO> articleList;
-    }
-
     @GetMapping("")
     public RsData<ArticlesResponse> list() {
         List<ArticleDTO> articleList = new ArrayList<>();
@@ -45,12 +39,6 @@ public class ApiV1ArticleController {
     }
 
 //    단건 조회
-    @Getter
-    @AllArgsConstructor
-    public static class ArticleResponse {
-        private final ArticleDTO article;
-    }
-
     @GetMapping("/{id}")
     public RsData<ArticleResponse> getArticle(@PathVariable("id") Long id) {
         Article article = new Article("제목1", "내용1");
@@ -60,33 +48,25 @@ public class ApiV1ArticleController {
     }
 
 //    등록
-    @Data
-    public static class ArticleRequest {
-        @NotBlank
-        private String subject;
-
-        @NotBlank
-        private String content;
-    }
-
     @PostMapping("")
-    public String create(@Valid@RequestBody ArticleRequest articleRequest) {
-        System.out.println(articleRequest.subject);
-        System.out.println(articleRequest.content);
+    public String create(@Valid@RequestBody ArticleCreateRequest articleCreateRequest) {
+        System.out.println(articleCreateRequest.getSubject());
+        System.out.println(articleCreateRequest.getContent());
 
         return "등록";
     }
 
 //    수정
     @PatchMapping("/{id}")
-    public String modify(@Valid@RequestBody ArticleRequest articleRequest) {
-        System.out.println(articleRequest.getSubject());
-        System.out.println(articleRequest.getContent());
+    public String modify(@PathVariable("id") Long id, @Valid @RequestBody ArticleModifyRequest articleModifyRequest) {
+        System.out.println(id);
+        System.out.println(articleModifyRequest.getSubject());
+        System.out.println(articleModifyRequest.getContent());
 
         return "수정";
     }
 
-
+//    삭제
     @DeleteMapping("{id}")
     public String delete(@PathVariable("id") Long id) {
         System.out.println(id);
