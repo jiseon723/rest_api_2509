59687f5f4a96d5a34d1201828215e6f8f3ae1773
jiseon723 <dighxxx12@gmail.com>
2025-09-19 16:27:36 +0900

    api v1 controller get CUD, RsData

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -3,6 +3,9 @@ package com.example.demo.article.controller;
 import com.example.demo.article.dto.ArticleDTO;
 import com.example.demo.article.entity.Article;
 import com.example.demo.article.service.ArticleService;
+import com.example.demo.global.RsData.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,8 +18,14 @@ import java.util.List;
 public class ApiV1ArticleController {
     private final ArticleService articleService;
 
+    @Getter
+    @AllArgsConstructor
+    public static class ArticlesResponse {
+        private final List<ArticleDTO> articleList;
+    }
+
     @GetMapping("")
-    public List<ArticleDTO> list () {
+    public RsData<ArticlesResponse> list() {
         List<ArticleDTO> articleList = new ArrayList<>();
 
         Article article1 = new Article("제목1", "내용1");
@@ -28,29 +37,44 @@ public class ApiV1ArticleController {
         Article article3 = new Article("제목3", "내용3");
         articleList.add(new ArticleDTO(article3));
 
-        return articleList;
+        return RsData.of("200", "게시글 다건 조회 성공", new ArticlesResponse(articleList)); //ArticlesResponse로 감싸는 이유 프론트엔드 작업자가 확인하기 좋음
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class ArticleResponse {
+        private final ArticleDTO article;
     }
 
     @GetMapping("/{id}")
-    public ArticleDTO getArticle (@PathVariable("id") Long id) {
+    public RsData<ArticleResponse> getArticle(@PathVariable("id") Long id) {
         Article article = new Article("제목1", "내용1");
         ArticleDTO articleDTO = new ArticleDTO(article);
 
-        return articleDTO;
+        return RsData.of("200", "게시글 다건 조회 성공", new ArticleResponse(articleDTO)); //ArticlesResponse로 감싸는 이유 프론트엔드 작업자가 확인하기 좋음
     }
 
     @PostMapping("")
-    public String create () {
+    public String create(@RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(subject);
+        System.out.println(content);
+
         return "등록";
     }
 
     @PatchMapping("/{id}")
-    public String modify () {
+    public String modify(@PathVariable("id") Long id, @RequestParam("subject") String subject, @RequestParam("content") String content) {
+        System.out.println(id);
+        System.out.println(subject);
+        System.out.println(content);
+
         return "수정";
     }
 
-    @DeleteMapping("/{id}")
-    public String delete () {
+    @DeleteMapping("{id}")
+    public String delete(@PathVariable("id") Long id) {
+        System.out.println(id);
+
         return "삭제";
     }
 }
