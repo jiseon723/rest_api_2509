d03a430a002529cc1cd813d29468603c0900b17f
jiseon723 <dighxxx12@gmail.com>
2025-09-22 16:22:27 +0900

    spring swagger

--- src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
+++ src/main/java/com/example/demo/article/controller/ApiV1ArticleController.java
@@ -10,20 +10,26 @@ import com.example.demo.article.response.ArticleResponse;
 import com.example.demo.article.response.ArticlesResponse;
 import com.example.demo.article.service.ArticleService;
 import com.example.demo.global.RsData.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 @RestController
-@RequestMapping("/api/v1/articles")
+@RequestMapping(value = "/api/v1/articles", produces = APPLICATION_JSON_VALUE, consumes = APPLICATION_JSON_VALUE) //무조건 JASON 으로 주고받는다.
 @RequiredArgsConstructor //final 사용 가능
+@Tag(name = "ApiV1ArticleController", description = "게시글 CRUD API")
 public class ApiV1ArticleController {
     private final ArticleService articleService;
 
 //    다건 조회
     @GetMapping("")
+    @Operation(summary = "게시글 다건 조회")
     public RsData<ArticlesResponse> list() {
         List<ArticleDTO> articleList = articleService.getList();
 
@@ -32,6 +38,7 @@ public class ApiV1ArticleController {
 
 //    단건 조회
     @GetMapping("/{id}")
+    @Operation(summary = "게시글 단건 조회")
     public RsData<ArticleResponse> getArticle(@PathVariable("id") Long id) {
         Article article = articleService.getArticle(id);
         ArticleDTO articleDTO = new ArticleDTO(article);
@@ -41,6 +48,7 @@ public class ApiV1ArticleController {
 
 //    등록
     @PostMapping("")
+    @Operation(summary = "게시글 등록")
     public RsData<ArticleCreateResponse> create(@Valid @RequestBody ArticleCreateRequest articleCreateRequest) {
         Article article = articleService.write(articleCreateRequest.getSubject(), articleCreateRequest.getContent());
 
@@ -49,6 +57,7 @@ public class ApiV1ArticleController {
 
 //    수정
     @PatchMapping("/{id}")
+    @Operation(summary = "게시글 수정")
     public RsData<ArticleModifyResponse> modify(@PathVariable("id") Long id, @Valid @RequestBody ArticleModifyRequest articleModifyRequest){
         Article article = articleService.getArticle(id);
 
@@ -65,6 +74,7 @@ public class ApiV1ArticleController {
 
 //    삭제
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제")
     public RsData<ArticleResponse> delete(@PathVariable("id") Long id) {
         Article article = articleService.getArticle(id);
 
