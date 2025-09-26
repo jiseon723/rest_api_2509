c5f2a9b7b193cc81121a1bda16bfb6f8cd049f8e
jiseon723 <dighxxx12@gmail.com>
2025-09-26 17:28:03 +0900

    react router 수정

--- frontapp/src/pages/Login.jsx
+++ frontapp/src/pages/Login.jsx
@@ -1,18 +1,19 @@
-function Login () {
-    <>
-        <h1>로그인</h1>
-        <form action="">
-            <label>
-                <span>로그인 아이디: </span>
-                <input type="text" />
-            </label>
-            <br />
-            <label>
-                <span>로그인 비밀번호: </span>
-                <input type="password" />
-            </label>
-            <button>로그인</button>
-        </form>
-    </>
+export default function Login () {
+    return (
+        <>
+            <h1>로그인</h1>
+            <form action="">
+                <label>
+                    <span>로그인 아이디: </span>
+                    <input type="text" />
+                </label>
+                <br />
+                <label>
+                    <span>로그인 비밀번호: </span>
+                    <input type="password" />
+                </label>
+                <button>로그인</button>
+            </form>
+        </>
+    )
 }
-export default Login
