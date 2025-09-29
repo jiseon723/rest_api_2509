c4ca2f2556fb1dd9c876fa7c2180873a7d00f7c2
jiseon723 <dighxxx12@gmail.com>
2025-09-29 14:49:48 +0900

    git fetch article list

--- frontapp/src/pages/ArticleList.jsx
+++ frontapp/src/pages/ArticleList.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
-export default function ArticleList () { // 짧을 때 사용
-    const [ArticleList, setArticleList] = useState()
+function ArticleList () { // 짧을 때 사용
+    const [articleList, setArticleList] = useState([])
 
-    fetch("http//localhost:8090/api/v1/aricles")
-        .then((res) => res.json())
-        .then((res) => {
-            console.log(res)
-        })
+    useEffect(() => {
+        fetch("http//localhost:8090/api/v1/aricles")
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res.data.articleList)
+                setArticleList(res.data.articleList)
+            })
+    }, [])
 
     return (
         <>
             <ul>
-                {ArticleList.map((article) => (
-                    <li key={article.id}>{article.subject}</li>
+                {articleList.map((article) => (
+                    <li key={article.id}>
+                        <span>{article.id}</span>
+                        <span>{article.subject}</span>
+                        <span>{article.content}</span>
+                        <span>{article.author}</span>
+                    </li>
                 ))}
             </ul>
         </>
     )
 }
+export default ArticleList
